1d6eec606a962596503fbb137b139f2bbe5afaf2
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-09 22:57:50 +0300

    Task001 Square without verification of data

---
+++ Task001_square/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите целое число и нажмите клавишу Enter: ");
+string s = Console.ReadLine();
+Console.WriteLine(s);
+int n = int.Parse(s);
+int result = n * n;
+Console.WriteLine(result);
