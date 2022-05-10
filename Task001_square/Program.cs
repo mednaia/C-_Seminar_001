16f901afd7a4304ff34c4d010ec604fa7039cc7d
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 10:38:27 +0300

    Task001 optimization

--- Task001_square/Program.cs
+++ Task001_square/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.WriteLine("Введите целое число и нажмите клавишу Enter: ");
-string s = Console.ReadLine();
-Console.WriteLine(s);
-int n = int.Parse(s);
+int n = int.Parse(Console.ReadLine());
 int result = n * n;
+Console.WriteLine("Квадрат числа: ");
 Console.WriteLine(result);
