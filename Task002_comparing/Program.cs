22e95244a30f66290ac59315c52e25e2411e9e6e
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 10:58:33 +0300

    Task 002 Comparing

---
+++ Task002_comparing/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите клавишу Enter: ");
+int b = int.Parse(Console.ReadLine());
+if(a > b)
+     Console.WriteLine($"Max: {a}  Min: {b}");
+else 
+     Console.WriteLine($"Max: {b}  Min: {a}");
