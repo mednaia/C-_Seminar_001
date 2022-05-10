07e5893d822a7359c6714ca6b13998785d00e1b4
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 11:39:26 +0300

    Task 004 Series of numbers

---
+++ Task004_series_of_numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+int start = - a;
+int end = a;
+while(start<=end)
+{
+    Console.WriteLine(start);
+    start = start +1;
+}
