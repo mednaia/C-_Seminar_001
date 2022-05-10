b5a5276b45b80f08d00724dac37cd7f2a85aa6f1
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 11:08:53 +0300

    Task 003 Compering of three numbers

---
+++ Task003_comparing_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите клавишу Enter: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число и нажмите клавишу Enter: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.WriteLine($"Max: {max}");
