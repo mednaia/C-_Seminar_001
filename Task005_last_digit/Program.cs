fb716857907358e8ab3ce88bd4613c0966114ab2
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 11:56:35 +0300

    Task 005 Last digit

---
+++ Task005_last_digit/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+if(a > 99 && a < 1000)
+{
+    Console.WriteLine(a % 10);
+}
+else
+{
+    Console.WriteLine("Это не трехзначное число!");
+}
