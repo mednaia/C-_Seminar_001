05c028c770268081623850faa579b6c02072c0cf
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-15 22:51:28 +0300

    task006 multyplicity 2 and 7

---
+++ Task006_multiplicity/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0 && a % 7 == 0)
+{
+    Console.WriteLine($"{a} кратно 2 и 7.");
+}
+else
+{
+    Console.WriteLine($"{a} не кратно 2 и 7.");
+}
